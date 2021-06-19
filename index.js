d641b1ef72bf0db368ceaced65f5d58cca344f7a
Harshith Kurapati <harshith.krpt@gmail.com>
2021-06-19 17:10:10 +0530

    S

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const _ = require("lodash")
 class E {
     s() {
         console.log("Hello World")
+        const sum  = _.add(10,20)
+
+        if(sum == 30) {
+            console.log("Passed");
+        }
+        else {
+            throw "Error";
+        }
+
     }
 }
 
